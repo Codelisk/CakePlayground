a0441e75422e106c511f2a1efc5da5ac477edd36
Daniel Hufnagl <d.hufnagl@codelisk.com>
2022-11-10 09:35:56 +0100

    Git init

---
+++ Build/build/Program.cs
@@ -0,0 +1,81 @@
+using System.Threading.Tasks;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Frosting;
+using Cake.Common;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Build;
+using Cake.Common.Tools.DotNet.Test;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        return new CakeHost()
+            .UseContext<BuildContext>()
+            .Run(args);
+    }
+}
+
+public class BuildContext : FrostingContext
+{
+    public string MsBuildConfiguration { get; set; }
+
+    public BuildContext(ICakeContext context)
+        : base(context)
+    {
+        MsBuildConfiguration = context.Argument("configuration", "Debug");
+    }
+}
+
+[TaskName("Clean")]
+public sealed class CleanTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.CleanDirectory($"../App1/App1/bin/{context.MsBuildConfiguration}");
+    }
+}
+
+[TaskName("Restore")]
+[IsDependentOn(typeof(CleanTask))]
+public sealed class RestoreTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetRestore("../App1/App1.sln");
+    }
+}
+
+[TaskName("Build")]
+[IsDependentOn(typeof(RestoreTask))]
+public sealed class BuildTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetBuild("../App1/App1.sln", new DotNetBuildSettings
+        {
+            Configuration = context.MsBuildConfiguration,
+        });
+    }
+}
+
+[TaskName("Test")]
+[IsDependentOn(typeof(BuildTask))]
+public sealed class TestTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetTest("../App1/App1.sln", new DotNetTestSettings
+        {
+            Configuration = context.MsBuildConfiguration,
+            NoBuild = true,
+        });
+    }
+}
+
+[IsDependentOn(typeof(TestTask))]
+public sealed class Default : FrostingTask
+{
+}
